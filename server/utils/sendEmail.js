033559bbd4dacc652540dae647bccbaba000625f
nelsonjhangi16 <nelosnjhangi5@gmail.com>
2026-06-07 23:24:57 +0500

    Switch email to Brevo SMTP

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,16 +1,18 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host:   "smtp-relay.brevo.com",
+  port:   587,
+  secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
   },
 });
 
 const sendEmail = async ({ to, subject, html }) => {
   await transporter.sendMail({
-    from: `"Expense Tracker" <${process.env.EMAIL_USER}>`,
+    from: `"Expense Tracker" <${process.env.BREVO_USER}>`,
     to,
     subject,
     html,
